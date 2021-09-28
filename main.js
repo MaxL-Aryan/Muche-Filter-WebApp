2ef6599bb792d073f4dc8ae5b38d886e43476d55
MaxL-Aryan <79088145+MaxL-Aryan@users.noreply.github.com>
2021-09-28 19:14:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+noseX=0;
+noseY=0;
+
+
+function preload()
+{
+    muche=loadImage('https://i.postimg.cc/T3wqXZKH/muche.png');
+}
+
+function setup()
+{
+canvas=createCanvas(380,300);
+canvas.position(500,280);
+video=createCapture(VIDEO);
+    video.hide();
+    tint_colour="";
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+image(video,0,0,380,300);
+tint(tint_colour);
+
+image(muche,noseX,noseY,40,32);
+}
+
+function take_snapshot()
+{
+    save('aryans_jadu.png');
+}
+
+function filter_colour()
+{
+    tint_colour=document.getElementById("filter_colour").value;
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x+10;
+        noseY=results[0].pose.nose.y-8;
+        console.log("Nose X = "+noseX);
+        console.log("Nose Y = "+noseY);
+    }
+}
+
+function modelLoaded()
+{
+    console.log("PoseNET is Initialize");
+}
